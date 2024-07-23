645f9e0c7e938b794de435b9621d119c5bab8fb5
Jingbo Su <sujingbo0217@gmail.com>
2024-07-23 00:42:57 -0700

    trivial modification

--- map/direct.hpp
+++ map/direct.hpp
@@ -14,79 +14,101 @@
 
 namespace ANN::map{
 
-template<typename Pid, typename Nid>
-class direct{
-	std::unordered_map<Nid,Pid> mapping;
-	static Nid f(const Pid &pid){
-		static_assert(std::is_convertible_v<size_t,Nid>);
-		return std::hash<Pid>{}(pid);
-	}
-public:
-	template<typename Iter>
-	void insert(Iter begin, Iter end){
-		// use mapping.insert_range() in C++23
-		const auto n = std::distance(begin, end);
-		auto ps = util::delayed_seq(n, [&](size_t i){
-			auto &&pid = *(begin+i);
-			return std::pair<Nid,Pid>(
-				f(pid), std::forward<decltype(pid)>(pid)
-			);
-		});
-		mapping.insert(ps.begin(), ps.end());
-	}
-	template<class Ctr>
-	void insert(Ctr &&c){
-		if constexpr(std::is_rvalue_reference_v<Ctr&&>)
-		{
-			insert(
-				std::make_move_iterator(c.begin()),
-				std::make_move_iterator(c.end())
-			);
-		}
-		else insert(c.begin(), c.end());
-	}
+  template<typename Pid, typename Nid>
+  class direct {
+    std::unordered_map<Nid, Pid> mapping;
+    // std::unordered_map<Pid, Nid> rev_mapping;
 
-	Nid insert(const Pid &pid){
-		return mapping.insert({f(pid),pid}).first->first;
-	}
-	Nid insert(Pid &&pid){
-		return mapping.insert({f(pid),std::move(pid)}).first->first;
-	}
+    static Nid f(const Pid &pid) {
+      static_assert(std::is_convertible_v<size_t, Nid>);
+      return std::hash<Pid>{}(pid);
+    }
 
-	template<typename Iter>
-	void erase(Iter begin, Iter end){
-		for(auto it=begin; it!=end; ++it)
-			mapping.erase(f(*it));
-	}
+   public:
+    template<typename Iter>
+    void insert(Iter begin, Iter end) {
+      // use mapping.insert_range() in C++23
+      const auto n = std::distance(begin, end);
 
-	void erase(Nid nid){
-		mapping.erase(nid);
-	}
+      auto ps = util::delayed_seq(n, [&](size_t i) {
+        auto &&pid = *(begin + i);
+        return std::pair<Nid, Pid>(f(pid), std::forward<decltype(pid)>(pid));
+      });
+      mapping.insert(ps.begin(), ps.end());
 
-	Pid get_pid(Nid nid) const{
-		return mapping.find(nid)->second;
-	}
-	Nid get_nid(const Pid &pid) const{
-		return f(pid);
-	}
+      // auto rps = util::delayed_seq(n, [&](size_t i) {
+      //   auto &&pid = *(begin + i);
+      //   return std::pair<Pid, Nid>(std::forward<decltype(pid)>(pid), f(pid));
+      // });
+      // rev_mapping.insert(rps.begin(), rps.end());
+    }
 
-	Nid front_nid() const{
-		return mapping.begin()->first;
-	};
+    template<class Ctr>
+    void insert(Ctr &&c) {
+      if constexpr (std::is_rvalue_reference_v<Ctr &&>) {
+        insert(std::make_move_iterator(c.begin()), std::make_move_iterator(c.end()));
+      } else {
+        insert(c.begin(), c.end());
+      }
+    }
 
-	// TODO: consider to remove it
-	std::optional<Nid> find_nid(const Pid &pid) const{
-		auto it = mapping.find(f(pid));
-		if(it==mapping.end())
-			return std::nullopt;
-		return {it->first};
-	}
+    Nid insert(const Pid &pid) {
+      // rev_mapping.insert({pid, f(pid)});
+      return mapping.insert({f(pid), pid}).first->first;
+    }
 
-	// TODO: use a more unambiguous name
-	bool contain_nid(Nid nid) const{
-		return mapping.contains(nid);
-	}
-};
+    Nid insert(Pid &&pid) {
+      // rev_mapping.insert({std::move(pid), f(pid)});
+      return mapping.insert({f(pid), std::move(pid)}).first->first;
+    }
+
+    template<typename Iter>
+    void erase(Iter begin, Iter end) {
+      for (auto it = begin; it != end; ++it) {
+				mapping.erase(f(*it));
+				// rev_mapping.erase(*it);
+			}
+    }
+
+    void erase(Nid nid) {
+      mapping.erase(nid);
+			// rev_mapping.erase(get_pid(nid));
+    }
+
+    Pid get_pid(Nid nid) const {
+      static_assert(std::is_convertible_v<Nid, Pid>);
+      return Pid(nid);
+    }
+
+    Nid get_nid(const Pid &pid) const {
+      static_assert(std::is_convertible_v<Pid, Nid>);
+      return Nid(pid);
+    }
+
+    Nid front_nid() const {
+      return mapping.begin()->first;
+    };
+
+    // TODO: consider to remove it
+    std::optional<Nid> find_nid(const Pid &pid) const {
+      auto it = mapping.find(f(pid));
+      if (it == mapping.end()) return std::nullopt;
+      return {it->first};
+    }
+
+    // TODO: use a more unambiguous name
+    bool contain_nid(Nid nid) const {
+      return mapping.contains(nid);
+    }
+
+    bool has_node(const Nid &nid) {
+      return (mapping.find(nid) != mapping.end());
+    }
+
+    std::unordered_map<Pid, Nid> get_map() const {
+      return mapping;
+    }
+  };
 
 } // namespace ANN::map
 
